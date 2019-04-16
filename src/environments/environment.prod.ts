5ca11095b3cb8192688fa4202fc93d7dc023df33
zios07 <Zios1997>
2019-04-16 01:26:16 +0100

    fix env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
 	production: true,
-	isMockEnabled: false, // You have to switch this, when your real back-end is done
+	isMockEnabled: true, // You have to switch this, when your real back-end is done
+	baseUrl: '',
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
 	production: false,
 	isMockEnabled: false, // You have to switch this, when your real back-end is done
-	baseUrl: 'http://localhost:8088/',
+	baseUrl: 'http://localhost:8089/',
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
